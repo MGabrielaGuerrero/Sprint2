013139663cad8593def48a0e0a9f5e6990823239
lCamiol <carlosorr@unicauca.edu.co>
2023-03-21 21:20:19 -0500

    [FEATURE] Logo and menu

--- src/components/header/Headers.jsx
+++ src/components/header/Headers.jsx
@@ -1,11 +1,28 @@
 import React from "react";
 import styles from "./Header.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGlobe } from "@fortawesome/free-solid-svg-icons";
 
 function Header() {
   return (
     <div className={styles.div}>
-      <h1 className={styles.titulo}>Header</h1>
-      <h1>Abiela fea</h1>
+      <div className={styles.divlogo}>
+        <img className={styles.logo} src="../images/nhoteles.png" alt="" />
+      </div>
+      <div className={styles.caja}>
+        <select name="language" id="language">
+          <option value=""><FontAwesomeIcon icon={faGlobe} />Idioma</option>
+          <option value="">English</option>
+          <option value="">Español</option>
+          <option value="">Français</option>
+          <option value="">Português</option>
+        </select>
+        <ul>
+          <li>Anunciar un hotel</li>
+          <li>Ayuda</li>
+          <li>Iniciar sesión</li>
+        </ul>
+      </div>
     </div>
   );
 }
